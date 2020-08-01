979d12cba719ae1b2c555084311e6285ec2aa448
super-180 <562889167@qq.com>
2020-08-01 16:25:54 +0800

    你的备注信息

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject bullet;
+    public float speed = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject b=GameObject.Instantiate(bullet, transform.position, transform.rotation);
+            Rigidbody rgd = b.GetComponent<Rigidbody>();
+            rgd.velocity = transform.forward * speed;
+        }
+    }
+}
